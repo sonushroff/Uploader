46cf54f655ac66716ec075234fdba36e151ba599
sonushroff <135789068+sonushroff@users.noreply.github.com>
2025-12-09 20:03:25 +0530

    Update vars.py

--- Modules/vars.py
+++ Modules/vars.py
@@ -1,26 +1,8 @@
-#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
-# Add your details here and then deploy by clicking on HEROKU Deploy button
-import os
-from os import environ
+# vars.py
 
-API_ID = int(environ.get("API_ID", "26271673"))
-API_HASH = environ.get("API_HASH", "0e807111856890e4770b3e5a3324ec5f")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8269699548:AAEUY3wP9W6lKt0XLQA_aHbPw_MUhT_pXbw")
+API_ID = 26271673     # apna API ID (my.telegram.org se)
+API_HASH = "0e807111856890e4770b3e5a3324ec5f"
+BOT_TOKEN = "8022061350:AAEmW1wboYHiz58BgRu7_HPAOU5orzrTWfA"
 
-OWNER = int(environ.get("OWNER", "820017857"))
-CREDIT = environ.get("CREDIT", "COURSEWALLA")
-cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
-
-TOTAL_USER = os.environ.get('TOTAL_USERS', '820017857').split(',')
-TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
-
-AUTH_USER = os.environ.get('AUTH_USERS', '820017857').split(',')
-AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
-if int(OWNER) not in AUTH_USERS:
-    AUTH_USERS.append(int(OWNER))
-
-
-
-
-
-
+# yahi naam sab jagah dikhaya jayega
+CREDIT = "COURSE WALLAH"
