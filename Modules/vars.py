3ee98a42acd22bdcfd7217344b1d793914256760
sonushroff <135789068+sonushroff@users.noreply.github.com>
2025-12-11 16:21:58 +0530

    Update vars.py

--- Modules/vars.py
+++ Modules/vars.py
@@ -2,7 +2,8 @@
 
 API_ID = 26271673     # apna API ID (my.telegram.org se)
 API_HASH = "0e807111856890e4770b3e5a3324ec5f"
-BOT_TOKEN = "8022061350:AAEmW1wboYHiz58BgRu7_HPAOU5orzrTWfA"
+BOT_TOKEN = "8276591766:AAFDGk1RUJPPLsXs-pxk5aHxreKHYIskS3w"
 
 # yahi naam sab jagah dikhaya jayega
 CREDIT = "COURSE WALLAH"
+
